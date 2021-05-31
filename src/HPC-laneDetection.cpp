807051214a6f03c7060e8d46deba65d78d91d644
Taylor Eyler Kaur <tcumings@uw.edu>
2021-05-31 10:26:54 -0700

    Separate C++ and CUDA code

---
+++ src/HPC-laneDetection.cpp
@@ -0,0 +1,8 @@
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
--- src/kernel.cu
+++ src/kernel.cu
@@ -1,10 +1,2 @@
 ﻿#include "cuda_runtime.h"
 #include "device_launch_parameters.h"
-#include "canny_unoptimized.h"
-#include <opencv2/highgui/highgui.hpp>
-#include "opencv2/imgproc/imgproc.hpp"
-
-int main()
-{
-    return 0;
-}
