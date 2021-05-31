7d6c96d2c67db4f625ce277c63eb8a6426594010
Taylor Eyler Kaur <tcumings@uw.edu>
2021-05-31 12:03:00 -0700

    Implement frame extraction function

--- src/HPC-laneDetection.cpp
+++ src/HPC-laneDetection.cpp
@@ -1,8 +1,44 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv2/imgproc/imgproc.hpp"
+#include "ed_pixel.h"
 #include <iostream>
 
-int main()
+void extractFrames(const std::string& videoFilePath, std::vector<cv::Mat>& framesOut)
 {
-    std::cout << "Hello World!\n";
+    try
+    {
+        cv::VideoCapture cap(videoFilePath);
+        if (!cap.isOpened())
+        {
+            std::cerr << "Unable to open video file!" << std::endl;
+            return;
+        }
+        for (int frameNum = 0; frameNum < cap.get(cv::CAP_PROP_FRAME_COUNT); frameNum++)
+        {
+            cv::Mat frame;
+            cap >> frame;
+            framesOut.push_back(frame);
+            // VISUAL DEBUG: display each frame on screen
+            cv::imshow("Extracted Frame", frame);
+        }
+    }
+    catch (cv::Exception& e)
+    {
+        std::cerr << e.msg << std::endl;
+    }
+}
+
+int main(int argc, char** argv)
+{
+    std::string videoFilePath = argv[1];
+    std::vector<cv::Mat> framesOutput;
+    extractFrames(videoFilePath, framesOutput);
+
+    // Extract color values for each pixel in each frame
+    std::vector<std::vector<pixel_t>> pixelValues;
+    for (int i = 0; i < framesOutput.size(); i++)
+    {
+        // call some function to convert each element of frame<Mat> to 2D vector of pixel_t
+    }
+    return 0;
 }
