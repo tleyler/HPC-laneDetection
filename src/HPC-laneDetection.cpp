6d5cc5b1d609a178ecab784172b366616ca9229f
msorvik <43226638+msorvik@users.noreply.github.com>
2021-05-31 20:23:48 -0700

    Initial success at detecting edges in a frame.
    Initial success at detecting edges in a frame.
    The edge image is really busy though with tons of edges. Not sure if this function needs to be tuned or if the image needs to be tuned before being passed to it. TBD.
    
    Also, getting some weird messages in the console while running, despite the fact that it is working. These will need to be investigated

--- src/HPC-laneDetection.cpp
+++ src/HPC-laneDetection.cpp
@@ -2,7 +2,27 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 
+using namespace cv;
+
+
+Mat opencvCanny(const Mat& frame) {
+
+    Mat edgeDetectedFrame;
+    Canny(frame, edgeDetectedFrame, 1, 3, 3);
+
+    return edgeDetectedFrame;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
+
+    Mat frame = imread("testingFrame.jpg");
+    imshow("Original Frame", frame);
+    imshow ("Edge Detected Frame", opencvCanny(frame));
+    waitKey(0);
+
+    std::cout << "Made it to the end" << std::endl;
 }
+
+
