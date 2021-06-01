1a5f3d10f7a9c1ecb5abbca49584a0ae29a7ae44
msorvik <43226638+msorvik@users.noreply.github.com>
2021-05-31 20:34:56 -0700

    Solved problem of detecting too many edges. Still need to investigate messages being output to the console

--- src/HPC-laneDetection.cpp
+++ src/HPC-laneDetection.cpp
@@ -4,25 +4,31 @@
 
 using namespace cv;
 
-
+// This function accepts a single frame and detects edges in it using opencv
+// Canny(). It returns the edge detected image.
 Mat opencvCanny(const Mat& frame) {
+    // changing this number effects the amount of edges that it detects. The 
+    // larger the number is, the less it will detect, only picking up larger 
+    // edges. Through some quick experimentation I settled on 75, but this can
+    // be adjusted later if we need more or less edges.
+    double edgeThreshold = 75.0;
 
     Mat edgeDetectedFrame;
-    Canny(frame, edgeDetectedFrame, 1, 3, 3);
+    Canny(frame, edgeDetectedFrame, edgeThreshold, edgeThreshold * 3.0, 3);
 
     return edgeDetectedFrame;
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
-
+    // pull the test image and display it
     Mat frame = imread("testingFrame.jpg");
     imshow("Original Frame", frame);
+    // also call the edge detection function that utilizes opencv Canny() and
+    // display the output of that also
     imshow ("Edge Detected Frame", opencvCanny(frame));
+    // wait for input, to give the user a chance to view images
     waitKey(0);
-
-    std::cout << "Made it to the end" << std::endl;
 }
 
 
