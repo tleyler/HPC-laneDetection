da5e77716ef4511ccb8bb3d9ef399faf40b55719
Taylor Eyler Kaur <tcumings@uw.edu>
2021-06-06 22:54:15 -0700

    Implement grayscale kernel

--- src/kernel.cu
+++ src/kernel.cu
@@ -1,2 +1,132 @@
-﻿#include "cuda_runtime.h"
+﻿#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/imgproc/imgproc.hpp"
+#include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <iostream>
+#include <vector>
+#define CHANNELS 3
+
+
+__global__ void grayscaleKernel(unsigned char* rgbInput, unsigned char* grayOutput, int width, int height, int colorWidthStep, int grayWidthStep)
+{
+    const int xIdx = blockIdx.x * blockDim.x + threadIdx.x;
+    const int yIdx = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (xIdx < width && yIdx < height)
+    {
+        const int colorOffset = yIdx * colorWidthStep + (CHANNELS * xIdx);
+        const int grayOffset = yIdx * grayWidthStep + xIdx;
+
+        const unsigned char b = rgbInput[colorOffset];
+        const unsigned char g = rgbInput[colorOffset + 1];
+        const unsigned char r = rgbInput[colorOffset + 2];
+
+        const float gray = r * 0.3f + g * 0.59f + b * 0.11f;
+        grayOutput[grayOffset] = static_cast<unsigned char>(gray);
+    }
+}
+
+void grayscaleCuda(const cv::Mat& hostInput, cv::Mat& hostOutput)
+{
+    // Allocate memory on device for input and output
+    unsigned char* deviceInput;
+    unsigned char* deviceOutput;
+    const int colorBytes = hostInput.step * hostInput.rows;
+    const int grayBytes = hostOutput.step * hostOutput.rows;
+    cudaMalloc<unsigned char>(&deviceInput, colorBytes);
+    cudaMalloc<unsigned char>(&deviceOutput, grayBytes);
+
+    // Copy memory from host to device
+    cudaMemcpy(deviceInput, hostInput.ptr(), colorBytes, cudaMemcpyHostToDevice);
+
+    // Call the kernel to convert the image to grayscale
+    const dim3 blockSize(16, 16, 1);
+    const dim3 gridSize((hostInput.cols + blockSize.x - 1) / blockSize.x, (hostInput.rows + blockSize.y - 1) / blockSize.y, 1);
+    grayscaleKernel << <gridSize, blockSize >> > (deviceInput, deviceOutput, hostInput.cols, hostInput.rows, hostInput.step, hostOutput.step);
+
+    // Copy memory back to host after kernel is complete
+    cudaDeviceSynchronize();
+    cudaMemcpy(hostOutput.ptr(), deviceOutput, grayBytes, cudaMemcpyDeviceToHost);
+    cudaFree(deviceInput);
+    cudaFree(deviceOutput);
+}
+
+// This function takes in a path to a video file (which are passed in as command line args to main)
+// as the first parameter and outputs each extracted frame to a vector of Mat items which is passed 
+// in as the second parameter to the function. 
+//extern "C++"
+void extractFrames(const std::string& videoFilePath, std::vector<cv::Mat>& framesOut)
+{
+    try
+    {
+        cv::VideoCapture cap(videoFilePath);
+        if (!cap.isOpened())
+        {
+            std::cerr << "Unable to open video file!" << std::endl;
+            return;
+        }
+        for (int frameNum = 0; frameNum < cap.get(cv::CAP_PROP_FRAME_COUNT); frameNum++)
+        {
+            cv::Mat frame;
+            cap >> frame;
+            framesOut.push_back(frame);
+            // VISUAL DEBUG: display each frame on screen
+            // cv::imshow("Extracted Frame", frame);
+            // waitKey(0);
+        }
+    }
+    catch (cv::Exception& e)
+    {
+        std::cerr << e.msg << std::endl;
+    }
+}
+
+
+// This function accepts a single frame and detects edges in it using opencv
+// Canny(). It returns the edge detected image.
+//extern "C++"
+cv::Mat opencvCanny(const cv::Mat& frame) {
+    // changing this number effects the amount of edges that it detects. The 
+    // larger the number is, the less it will detect, only picking up larger 
+    // edges. Through some quick experimentation I settled on 75, but this can
+    // be adjusted later if we need more or less edges.
+    double edgeThreshold = 75.0;
+
+    // this mat will hold the edges image
+    cv::Mat edgeDetectedFrame;
+    cv::Canny(frame, edgeDetectedFrame, edgeThreshold, edgeThreshold * 3.0, 3);
+
+    return edgeDetectedFrame;
+}
+
+// COMMAND LINE ARGUMENTS
+// argv[0] = program name
+// argv[1] = file path to video file
+//extern "C++"
+int main(int argc, char** argv)
+{
+    std::string videoFilePath = argv[1];
+    std::vector<cv::Mat> framesOutput;
+    extractFrames(videoFilePath, framesOutput);
+
+    for (int i = 0; i < framesOutput.size(); i++)
+    {
+        cv::Mat image = framesOutput[i];
+        const int rows = image.rows;
+        const int cols = image.cols;
+        //imshow("Extracted Frame", image);
+        //cv::waitKey(0);
+
+        // convert the image to grayscale 
+        cv::Mat grayscale = cv::Mat(rows, cols, CV_8UC1);
+        grayscaleCuda(image, grayscale);
+        imshow("Grayscale Image", grayscale);
+        cv::waitKey(0);
+
+        // Apply and output opencvCanny() to each extracted frame
+        //imshow("Edge Detected Frame", opencvCanny(image));
+        //waitKey(0);
+    }
+
+    return 0;
+}
